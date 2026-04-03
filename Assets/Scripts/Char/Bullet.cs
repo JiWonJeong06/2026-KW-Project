eaacdd51ebacf9eac11b8498de0f23adf28bdd4d
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2026-04-03 19:44:06 +0900

    캐릭터 사거리 및 펫 범위 공격

--- Assets/Scripts/Char/Bullet.cs
+++ Assets/Scripts/Char/Bullet.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damage = 1f;
+    public float range = 1f;
 
     Rigidbody2D rb;
+    Vector3 startPos;
 
     void Awake()
     {
@@ -13,19 +15,26 @@ public class Bullet : MonoBehaviour
 
     public void Init(Vector2 dir, float speed)
     {
+        startPos = transform.position;
         rb.linearVelocity = dir * speed;
+    }
+
+    void FixedUpdate()
+    {
+        if (Vector3.Distance(startPos, transform.position) >= range)
+        {
+            Destroy(gameObject);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // 데미지 가능한 대상 확인
         var damageTarget = collision.GetComponent<Enemy>();
         if (damageTarget != null)
         {
             damageTarget.TakeDamage(damage);
         }
 
-        // 어떤 물체든 부딪히면 총알 제거
         Destroy(gameObject);
     }
 }
