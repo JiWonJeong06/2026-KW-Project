580777c04f7fcfafa11c078e6b17feb2c3ce9666
JiWonJeong06 <152679525+JiWonJeong06@users.noreply.github.com>
2026-04-20 11:10:04 +0900

    데이터 테이블 삽입 및 적 더미 삽입

---
+++ Assets/2_Scripts/Data/AbilityData.cs
@@ -0,0 +1,14 @@
+[System.Serializable]
+public class AbilityData
+{
+    public int Number;
+    public string Type;
+    public string Name;
+    public string Rank;
+    public string MainAbility;
+    public string SubAbility;
+    public float Increase;
+    public float SubIncrease;
+    public string Explanation;
+    public string Text;
+}
